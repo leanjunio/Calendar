4021cd2ce2dd589e25eebded78211fc5a847ffd4
Lean Junio <leanjunio@live.com>
2018-10-30 17:43:40 -0400

    Finished post request for saving into backend

--- client/src/AddBooking.js
+++ client/src/AddBooking.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Axios from 'axios';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
@@ -31,53 +32,63 @@ class AddBooking extends Component {
     this.setState({ room: e.target.value })
   }
   handleSubmit() {
-    alert(`Date: ${this.state.startDate}`)
-    alert(`Room: ${this.state.room}`)
-    alert(`Owner: ${this.state.owner}`)
+    console.log({
+      date: this.state.date,
+      owner: this.state.owner,
+      room: this.state.room
+    })
+
+    Axios.post('http://localhost:8080/calendar', {
+      date: this.state.date,
+      owner: this.state.owner,
+      room: this.state.room
+    })
+      .then((res) => console.log(res))
+      .catch ((err) => console.log(err));
   }
-  render() {
-    return (
-      <Container>
-        <Form>
-          <FormGroup>
-            <Label for="Owner">Owner</Label>
-            <Input 
-              value={this.state.owner}
-              onChange={this.handleChangeOwner}
-              type="text" 
-              name="owner" 
-              id="idOwner" 
-              placeholder="John Doe"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Date">Date - Time (YYYY-MM-DDT00:00)</Label>
-            <DatePicker
-              selected={this.state.startDate}
-              onChange={this.handleChangeDate}
-              showTimeSelect
-              timeFormat="HH:mm"
-              timeIntervals={15}
-              dateFormat="LLL"
-              timeCaption="time"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="Room">Room</Label>
-            <Input 
-              value={this.state.room}
-              onChange={this.handleChangeRoom}
-              type="text" 
-              name="room" 
-              id="idRoom" 
-              placeholder="DB4040"
-            />
-          </FormGroup>
-          <Button onClick={this.handleSubmit}>Submit</Button>
-        </Form>
-      </Container>
-    )
-  }
+render() {
+  return (
+    <Container>
+      <Form>
+        <FormGroup>
+          <Label for="Owner">Owner</Label>
+          <Input
+            value={this.state.owner}
+            onChange={this.handleChangeOwner}
+            type="text"
+            name="owner"
+            id="idOwner"
+            placeholder="John Doe"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Date">Date - Time (YYYY-MM-DDT00:00)</Label>
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={this.handleChangeDate}
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={15}
+            dateFormat="LLL"
+            timeCaption="time"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Room">Room</Label>
+          <Input
+            value={this.state.room}
+            onChange={this.handleChangeRoom}
+            type="text"
+            name="room"
+            id="idRoom"
+            placeholder="DB4040"
+          />
+        </FormGroup>
+        <Button onClick={this.handleSubmit}>Submit</Button>
+      </Form>
+    </Container>
+  )
+}
 }
 
 export default AddBooking;
