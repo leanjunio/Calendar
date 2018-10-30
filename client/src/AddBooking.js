bd6c9a9c80fd101cca0a7768f322e7206c673966
Lean Junio <leanjunio@live.com>
2018-10-30 13:40:50 -0400

    Added component for AddBooking

--- client/src/AddBooking.js
+++ client/src/AddBooking.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import { Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 class AddBooking extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      startDate: moment()
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
   render() {
     return (
-      <div>
-        Add Booking Component
-      </div>
+      <Container>
+        <Form>
+          <FormGroup>
+            <Label for="Owner">Owner</Label>
+            <Input type="text" name="owner" id="idOwner" placeholder="John Doe" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="Date">Date - Time (YYYY-MM-DDT00:00)</Label>
+            <DatePicker
+              selected={this.state.startDate}
+              onChange={this.handleChange}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              dateFormat="LLL"
+              timeCaption="time"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="Room">Room</Label>
+            <Input type="text" name="room" id="idRoom" placeholder="DB4040" />
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
+      </Container>
     )
   }
 }
